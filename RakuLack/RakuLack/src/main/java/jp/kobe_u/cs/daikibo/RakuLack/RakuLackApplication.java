041a0add3ca87ae5fa193855f2cac4f9ba1d4216
tasuku <plusnine865@gmail.com>
2022-08-01 09:41:01 +0900

    プロジェクトを作成

---
+++ RakuLack/RakuLack/src/main/java/jp/kobe_u/cs/daikibo/RakuLack/RakuLackApplication.java
@@ -0,0 +1,13 @@
+package jp.kobe_u.cs.daikibo.RakuLack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RakuLackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakuLackApplication.class, args);
+	}
+
+}
