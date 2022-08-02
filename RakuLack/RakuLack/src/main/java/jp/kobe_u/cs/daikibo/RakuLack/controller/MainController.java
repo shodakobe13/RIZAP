8874170b42ef9a940ea0038d5606c28c7e5e9720
tasuku <plusnine865@gmail.com>
2022-08-02 14:51:38 +0900

    ログイン処理を修正

--- RakuLack/RakuLack/src/main/java/jp/kobe_u/cs/daikibo/RakuLack/controller/MainController.java
+++ RakuLack/RakuLack/src/main/java/jp/kobe_u/cs/daikibo/RakuLack/controller/MainController.java
@@ -1,18 +1,33 @@
 package jp.kobe_u.cs.daikibo.RakuLack.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import jp.kobe_u.cs.daikibo.RakuLack.form.UidForm;
 
 @Controller
 public class MainController {
-    @Autowired
     // タイトル画面を表示
-    @PostMapping("/main")
-    String showIndex(@RequestParam("userid") String userid, Model model) {
-        model.addAttribute("uid", userid);
-        return "{uid}/menu";
+    @GetMapping("/")
+    String showIndex(Model model) {
+        UidForm uform = new UidForm();
+        model.addAttribute("uidform", uform);
+        return "login";
+    }
+
+    @PostMapping("/login")
+    String handleLogin(Model model, @ModelAttribute UidForm form) {
+        String uid = form.getUid();
+        return "redirect:" + uid + "/menu";
+    }
+
+    @GetMapping("/{uid}/menu")
+    String showMenu(@PathVariable String uid, Model model) {
+        model.addAttribute("uid", uid);
+        return "menu";
     }
 }
